ced602120f7509e5703ba685b9e8faaa4de101b8
Movie1103 <tpawarut@gmail.com>
2022-06-22 14:02:45 +0700

    feat(login): add register, login

---
+++ controllers/authController.js
@@ -0,0 +1,64 @@
+const createError = require('../utils/createError');
+const bcrypt = require('bcryptjs');
+const { User } = require('../models');
+const jwt = require('jsonwebtoken');
+// const { Op } = require('sequelize');
+
+const genToken = payload =>
+  jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: { email },
+    });
+    if (!user) {
+      createError('invalid credential', 400);
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      createError('invalid credential', 400);
+    }
+
+    const token = genToken({ id: user.id });
+    res.status(200).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.register = async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, password, confirmPassword } = req.body;
+
+    if (!email) {
+      createError('email is required', 400);
+    }
+    if (!password) {
+      createError('password is required', 400);
+    }
+    if (password.length < 6) {
+      createError('password must be longer than 6 characters');
+    }
+    if (password !== confirmPassword) {
+      createError('password and confirm password did not match', 400);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = genToken({ id: user.id });
+    res.status(201).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
--- models/User.js
+++ models/User.js
@@ -1,102 +1,99 @@
-const sequelize = require('sequelize')
+const sequelize = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-
-    const User = sequelize.define(
-        'User', {
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
+  const User = sequelize.define(
+    'User',
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        email: {
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false,
-            validate: {
-                notEmpty: true,
-                isEmail: true
-            }
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          isEmail: true,
         },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        role: {
-            type: DataTypes.ENUM('User', 'Admin'),
-            defaultValue: ('User'),
-            allowNull: true
+      },
+      role: {
+        type: DataTypes.ENUM('User', 'Admin'),
+        defaultValue: 'User',
+        allowNull: true,
+      },
+      profilePicUrl: {
+        type: DataTypes.STRING,
+        defaultValue: 'https://picsum.photos/200',
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        profilePicUrl: {
-            type: DataTypes.STRING,
-            defaultValue: 'https://picsum.photos/200',
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        googleId: {
-            type: DataTypes.STRING,
-        }
-
-    }, {
-        underscored: true,
-        timestamps: false
+      },
+      googleId: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      underscored: true,
+      paranoid: true,
     }
-    )
+  );
 
-    User.associate = models => {
+  User.associate = models => {
+    User.hasMany(models.Restaurant, {
+      foreignKey: {
+        name: 'userId',
+        allowNull: false,
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
 
-        User.hasMany(models.Restaurant, {
-            foreignKey: {
-                name: 'userId',
-                allowNull: false
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'CASCADE'
-        })
+    User.hasMany(models.Like, {
+      foreignKey: {
+        name: 'userId',
+        allowNull: false,
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
 
-        User.hasMany(models.Like, {
-            foreignKey: {
-                name: 'userId',
-                allowNull: false
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'CASCADE'
-        })
+    User.hasMany(models.Comment, {
+      foreignKey: {
+        name: 'userId',
+        allowNull: false,
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
 
-        User.hasMany(models.Comment, {
-            foreignKey: {
-                name: 'userId',
-                allowNull: false
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'CASCADE'
-        })
+    User.hasMany(models.Pin, {
+      foreignKey: {
+        name: 'userId',
+        allowNull: false,
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
+  };
 
-        User.hasMany(models.Pin, {
-            foreignKey: {
-                name: 'userId',
-                allowNull: false
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'CASCADE'
-        })
-
-
-    }
-
-    return User
-}
+  return User;
+};
