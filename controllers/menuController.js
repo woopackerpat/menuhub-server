c17f1387e78667d32b80b887ceed2ba0deb43d1b
Movie1103 <tpawarut@gmail.com>
2022-06-22 20:13:19 +0700

    fixed: edited status code in restaurantController, removed check is deleted f() in menuController

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,106 +1,97 @@
-const { Restaurant, Menu } = require('../models')
-const createError = require('../utils/createError')
+const { Restaurant, Menu } = require('../models');
+const createError = require('../utils/createError');
 
 exports.createMenu = async (req, res, next) => {
-    try {
-        const restaurantId = req.params.restaurantid
-        const { title, imageUrl, description, orderNumber } = req.body
+  try {
+    const restaurantId = req.params.restaurantid;
+    const { title, imageUrl, description, orderNumber } = req.body;
 
-        // Validation
-        if (!title) {
-            createError('Requires a Title', 400)
-        }
-        if (!imageUrl) {
-            createError('Requires an Image', 400)
-        }
-        if (!description) {
-            createError('Requires a Description', 400)
-        }
-        if (!orderNumber) {
-            createError('Requires an Order Number', 400)
-        }
-        if (!restaurantId) {
-            createError('Requires a Restaurant ID', 400)
-        }
-
-        const newMenu = await Menu.create({
-            restaurantId,
-            title,
-            imageUrl,
-            description,
-            orderNumber,
-            include: {
-                model: Restaurant,
-                attributes: ["name"]
-            }
-        });
-
-        res.status(201).json({ newMenu });
-    } catch (err) {
-        next(err)
+    // Validation
+    if (!title) {
+      createError('Requires a Title', 400);
     }
-}
+    if (!imageUrl) {
+      createError('Requires an Image', 400);
+    }
+    if (!description) {
+      createError('Requires a Description', 400);
+    }
+    if (!orderNumber) {
+      createError('Requires an Order Number', 400);
+    }
+    if (!restaurantId) {
+      createError('Requires a Restaurant ID', 400);
+    }
+
+    const newMenu = await Menu.create({
+      restaurantId,
+      title,
+      imageUrl,
+      description,
+      orderNumber,
+      include: {
+        model: Restaurant,
+        attributes: ['name'],
+      },
+    });
+
+    res.status(201).json({ newMenu });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.updateMenu = async (req, res, next) => {
-    try {
-        
-        const menuId = req.params.menuid
+  try {
+    const menuId = req.params.menuid;
 
-        const { title, imageUrl, description, orderNumber } = req.body
+    const { title, imageUrl, description, orderNumber } = req.body;
 
-        const toUpdateMenu = await Menu.findOne({
-            where: {
-                id: menuId
-            },
-            include: {
-                model: Restaurant,
-                attributes: ["name"]
-            }
-        });
+    const toUpdateMenu = await Menu.findOne({
+      where: {
+        id: menuId,
+      },
+      include: {
+        model: Restaurant,
+        attributes: ['name'],
+      },
+    });
 
-        if (title) {
-            toUpdateMenu.title = title
-        } if (imageUrl) {
-            toUpdateMenu.imageUrl = imageUrl
-        } if (description) {
-            toUpdateMenu.description = description
-        } if (orderNumber) {
-            toUpdateMenu.orderNumber = orderNumber
-        }
-        
-        const updatedMenu = await toUpdateMenu.save()
-        
-        res.status(201).json({ updatedMenu, message: "Up to Date" });
-    } catch (err) {
-        next(err)
+    if (title) {
+      toUpdateMenu.title = title;
     }
-    
-}
+    if (imageUrl) {
+      toUpdateMenu.imageUrl = imageUrl;
+    }
+    if (description) {
+      toUpdateMenu.description = description;
+    }
+    if (orderNumber) {
+      toUpdateMenu.orderNumber = orderNumber;
+    }
+
+    const updatedMenu = await toUpdateMenu.save();
+
+    res.status(201).json({ updatedMenu, message: 'Up to Date' });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.destroyMenu = async (req, res, next) => {
-    try {
-        const menuId = req.params.menuid
+  try {
+    const menuId = req.params.menuid;
 
-        const menuToDelete = await Menu.findOne({
-            where: {
-                id: menuId
-            }
-        });
+    const menuToDelete = await Menu.findOne({
+      where: {
+        id: menuId,
+      },
+    });
 
-        menuToDelete.destroy()
+    menuToDelete.destroy();
 
-        const notDeleted = await Menu.findOne({
-            where: {
-                id: menuId
-            }
-        });
-
-        if (notDeleted) {
-            createError('Menu could not be deleted', 500)
-        }
-
-        res.status(201).json({ message: "Menu deleted" });
-    } catch (err) {
-        next(err)
-    }
-}
+    res.status(204).json();
+  } catch (err) {
+    next(err);
+  }
+};
