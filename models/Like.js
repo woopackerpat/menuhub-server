f1c3c252befc43279a5271f414567ec28ab4e20c
Movie1103 <tpawarut@gmail.com>
2022-06-22 14:21:54 +0700

    fix: remove unneccessary imports from all models

--- models/Like.js
+++ models/Like.js
@@ -1,35 +1,31 @@
-const sequelize = require('sequelize')
-
 module.exports = (sequelize, DataTypes) => {
-
-    const Like = sequelize.define(
-        'Like', {
-
-    }, {
-        underscored: true,
-        timestamps: false
+  const Like = sequelize.define(
+    'Like',
+    {},
+    {
+      underscored: true,
+      timestamps: false,
     }
-    )
+  );
 
-    Like.associate = models => {
+  Like.associate = models => {
+    Like.belongsTo(models.User, {
+      foreignKey: {
+        name: 'userId',
+        allowNull: false,
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
+    Like.belongsTo(models.Restaurant, {
+      foreignKey: {
+        name: 'restaurantId',
+        allowNull: false,
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
+  };
 
-        Like.belongsTo(models.User, {
-            foreignKey: {
-                name: 'userId',
-                allowNull: false
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'CASCADE'
-        })
-        Like.belongsTo(models.Restaurant, {
-            foreignKey: {
-                name: 'restaurantId',
-                allowNull: false
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'CASCADE'
-        })
-    }
-    
-    return Like
-}
+  return Like;
+};
