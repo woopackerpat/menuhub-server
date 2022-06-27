c3a1d985868b2b0915fac3b6d1f6aafea02dc322
ZonalDean <huber.deanc@gmail.com>
2022-06-27 16:03:35 +0700

    feat(controller): Added Like cycle

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,2 +1,37 @@
 const { Restaurant, Menu, User, Like } = require('../models');
 const createError = require('../utils/createError');
+
+exports.cycleComment = async (req, res, next) => {
+    try {
+        const userId = req.user.id
+        const restaurantId = req.params.restaurantid
+
+        const foundLike = await Like.findOne({
+            where: {
+                userId,
+                restaurantId
+            }
+        });
+
+        // const foundRestaurant = await Restaurant.findOne({
+        //     where: {
+        //         id: restaurantId
+        //     }
+        // });
+
+        if (!foundLike) {
+            await Like.create({
+                userId,
+                restaurantId
+            })
+            res.status(200).json({"Message": "Liked"})
+        }
+
+        if (foundLike) {
+            await foundLike.destroy()
+            res.status(200).json({"Message": "Like deleted"})
+        }
+    } catch (err) {
+        next(err)
+    }
+}
