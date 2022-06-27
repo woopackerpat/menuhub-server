ad6a2fb906172a773688e55af1ae45e7f9eca580
ZonalDean <huber.deanc@gmail.com>
2022-06-27 13:46:26 +0700

    feat(category): Modified restaurant controller for category, added cattegory routes

--- router/categoryRoute.js
+++ router/categoryRoute.js
@@ -3,3 +3,8 @@ const authenticate = require('../middlewares/authenticate');
 const categoryController = require('../controllers/categoryController')
 
 const router = express.Router();
+
+router.post('', categoryController.createCategory)
+router.patch('', categoryController.destroyCategory)
+
+module.exports = router
