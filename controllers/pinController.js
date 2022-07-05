7639d36b28ee88cfeb9c2ac9929931ab8a3d1fb0
ZonalDean <huber.deanc@gmail.com>
2022-07-05 20:25:11 +0700

    fix: MyPins order

--- controllers/pinController.js
+++ controllers/pinController.js
@@ -88,7 +88,7 @@ exports.getMyPins = async (req, res, next) => {
           attributes: ["imageUrl", "orderNumber"],
         },
       },
-      order: [[Restaurant, Menu, 'orderNumber', 'ASC']]
+      order: [['createdAt', 'ASC'], [Restaurant, Menu, 'orderNumber', 'ASC']]
     })
 
     res.status(200).json(myPins)
